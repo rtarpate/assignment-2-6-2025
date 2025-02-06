bb4e901a32e0af37fc53cbaccb0f3d153099f533
Ashton Pate <arpate@amarillocollege.com>
2025-02-06 14:03:02 -0600

    Add project files.

---
+++ assignment 2 6 2025/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2_6_2025
+{
+    internal class Program
+    {
+        //create a method to check if a number is even
+        //use this method to check and print out all even number in an array with 10 element form 1 to 10
+
+        
+
+
+        static bool IsEven(int aNumber)
+        {
+            bool retValue = false;
+
+            if (aNumber % 2 == 0)
+            {
+                retValue = true;
+            }
+
+            return  retValue;
+        }
+
+        static int[] CreateArray()
+        {
+            int[] retValue = new int[10];
+
+            Random ran = new Random();
+
+            for (int i = 0; i < retValue.Length; i++)
+            {
+                retValue[i] = ran.Next(1, 11);
+            }
+
+            
+            return  retValue;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = CreateArray();
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
